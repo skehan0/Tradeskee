1984bb7377ebf42147bf9e0d01e3a5df31db0eae
Gavin Skehan <g.skehan1@universityofgalway.ie>
2026-02-07 22:55:58 +0000

    refactor: Remove

--- src/feature/logging/logging_config.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-from src.feature.logging.applog import AppLogger
-from src.feature.logging.logging_interface import LoggingService
-from src.feature.logging.logging_handlers import StdoutLoggingService
-
-
-def build_app_logger(handlers: List[LoggingService]) -> AppLogger:
-    """Dependency injection composition root"""
-    return AppLogger(handlers=handlers)
